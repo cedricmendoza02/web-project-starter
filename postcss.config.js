c74c9ff24b601ba31d1fede6dda98e1c45cb7d49
Christian Pelayo <pelayochristian.dev@gmail.com>
2022-10-18 18:15:37 +0800

    tailwind pre-process with babel

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,4 @@
-/** @type {import('tailwindcss').Config} */
 const tailwindcss = require('tailwindcss');
-
 module.exports = {
-    content: ['./src/**/*.{html,js}'],
-    theme: {
-        extend: {},
-    },
     plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')],
 };
